582b28b4f42dbf02855e1965ec04bc46c69fd72f
Luis Alberto <luisalbertolaramartinez3c@gmail.com>
2025-11-28 19:55:10 +0100

    Login funcional acabado

--- kompu/src/app/pages/login/login.ts
+++ kompu/src/app/pages/login/login.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Inject, PLATFORM_ID } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { isPlatformBrowser } from '@angular/common';
+import { Usuarios } from '../../services/usuarios/usuarios';
 
 @Component({
   selector: 'app-login',
@@ -8,11 +10,42 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './login.css',
 })
 export class Login {
-  email = '';
-  clave = '';
+  email: string = '';
+  clave: string = '';
+
+  constructor(private servicio: Usuarios) { }
+
+  ngOnInit() {
+    this.cargarUsuarios();
+  }
+
+  cargarUsuarios() {
+    this.servicio.obtenerUsuarios().subscribe({
+      next: listaUsuarios => {
+        if (typeof window !== 'undefined') {
+          localStorage.setItem("listaUsuarios", JSON.stringify(listaUsuarios));
+        }
+      }
+    });
+  }
 
   onLogin() {
-    console.log('Usuario:', this.email);
-    console.log('Contraseña:', this.clave);
+    this.servicio.iniciarSesion(this.email, this.clave).subscribe({
+      next: token => {
+        alert("Token JWT: " + token);
+      },
+      error: err => {
+        if (err.status === 403) {
+          alert("Error: No se pudo acceder a la lista de usuarios.");
+          this.cargarUsuarios();
+          return;
+        }
+
+        if (err.status === 401) {
+          alert("Error: Credenciales incorrectas.");
+          return;
+        }
+      }
+    });
   }
 }
--- kompu/src/app/services/usuarios/usuarios.ts
+++ kompu/src/app/services/usuarios/usuarios.ts
@@ -1,16 +1,55 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Usuario } from '../../models/usuario';
+import { map, Observable, of, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Usuarios {
-  private URL_API = "assets/data/usuarios.json";
+  private claveSecreta = "luissamuelalejandrokompu";
+  private urlApi = "assets/data/usuarios.json";
 
   constructor(private http: HttpClient) { }
 
-  obtenerUsuarios() {
-    return this.http.get<Usuario[]>(this.URL_API);
+  obtenerUsuarios(): Observable<Usuario[]> {
+    return this.http.get<Usuario[]>(this.urlApi).pipe(
+      map(usuarios => {
+        return usuarios.map(usuario => {
+          return { ...usuario, clave: btoa(usuario.clave) };
+        });
+      })
+    );
+  }
+
+  iniciarSesion(email: string, clave: string): Observable<string> {
+    const listaUsuariosRaw = localStorage.getItem("listaUsuarios");
+    if (!listaUsuariosRaw) {
+      return throwError(() => new HttpErrorResponse({ status: 403 }));
+    }
+
+    const listaUsuarios: Usuario[] = JSON.parse(listaUsuariosRaw);
+
+    const usuario = listaUsuarios.find(u => u.email.toLowerCase() === email.toLowerCase() && u.clave === btoa(clave));
+
+    if (!usuario) {
+      return throwError(() => new HttpErrorResponse({ status: 401 }));
+    }
+
+    let header = btoa(JSON.stringify({
+      alg: "HS256",
+      typ: "JWT"
+    }));
+
+    let payload = btoa(JSON.stringify({
+      id: usuario.id,
+      userName: usuario.nombre,
+      rol: usuario.rol,
+      exp: Math.floor(Date.now() / 1000) + (5 * 3600)
+    }));
+
+    let firma = btoa(this.claveSecreta);
+
+    return of(`${header}.${payload}.${firma}`);
   }
 }
